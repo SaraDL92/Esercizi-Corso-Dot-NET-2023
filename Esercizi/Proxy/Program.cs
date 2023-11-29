dea9f9a97f83bc4e557b43ea05a207e8743ef67c
Sara Di Luca <150133964+SaraDL92@users.noreply.github.com>
2023-11-29 11:44:30 +0100

    Project Proxy added

---
+++ Esercizi/Proxy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace Proxy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Proxy proxy = Proxy.Instance;
+            Proxy proxy1 = Proxy.Instance;
+            Console.WriteLine("Istanza 1:");
+            proxy.randomnumb();
+            Console.WriteLine("------------");
+            Console.WriteLine("Istanza 2:");
+            proxy1.randomnumb();
+
+        }
+    }
+}
---
+++ Esercizi/Proxy/Proxy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+
+    internal class Proxy
+    {
+        private static Proxy _instance;
+        private static Random rnd=new Random();
+        private static byte first=(byte)rnd.Next(256);
+        private static byte second = (byte)rnd.Next(256);
+        private static byte third = (byte)rnd.Next(256);
+        private static byte fourth = (byte)rnd.Next(256);
+        private static byte first1 = (byte)rnd.Next(256);
+        private static byte second1 = (byte)rnd.Next(256);
+        private static byte third1 = (byte)rnd.Next(256);
+        private static byte fourth1 = (byte)rnd.Next(256);
+
+
+        private Proxy() {
+           
+        }
+        public void randomnumb()
+        {
+            Console.WriteLine($"Primo Indirizzo IP:{first}.{second}.{third}.{fourth}");
+            Console.WriteLine($"Secondo Indirizzo IP:{first1}.{second1}.{third1}.{fourth1}");
+
+        }
+        public static Proxy Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Proxy();
+                }
+                return _instance;
+            }
+        }
+      
+    }
+}
+
+
+
+
+
+
